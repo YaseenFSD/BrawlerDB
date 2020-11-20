232b9af3151b1418dc834bfeedd3d9415e05bd61
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-11-20 09:44:46 -0500

    added proxy to client

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,11 +2,11 @@ import axios from "axios"
 
 
 const axiosInstance = axios.create({
-    baseURL: `http://localhost:${process.env.REACT_APP_BACKEND_PORT}`
+    baseURL: `http://localhost:5000`
 })
 
 export const getRanks = async(key, brackets, region, page) => {
     // const ranks = axiosInstance.get(`rankings/${brackets}/${region}/${page}?api_key=${process.env.REACT_APP_API_KEY}`)
-    const ranks = await axiosInstance.get(`/api/rankings/${brackets}/${region}/${page}`)
+    const ranks = await axios.get(`/api/rankings/${brackets}/${region}/${page}`)
    return ranks
 }
