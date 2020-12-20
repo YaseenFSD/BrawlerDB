04c8582ef6547f3725e843af605bc11ca2e16a6c
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-12-20 17:20:56 -0500

    2v2 bracket filter now works

--- client/src/components/leaderboad-table/LeaderboardTable.js
+++ client/src/components/leaderboad-table/LeaderboardTable.js
@@ -1,28 +1,53 @@
 import { cleanString } from "../../helpers"
+import { getPlayerName } from "../../api"
 
-export const LeaderboardTable = ({players, brackets}) => {
-    const splitTeamNames = (teamname) => {
-        // let players = teamname.split("+")
-        // if (players.length === 2) {
-        //     return players
-        // } else {
+export const LeaderboardTable = ({ players, brackets }) => {
 
-        // }
+
+    const splitTeamNames = (teamname, playerOneId, playerTwoId) => {
+        let players = teamname.split("+")
+        // * This is just in case one of the players has a '+' symbol in their name
+        if (players.length === 2) {
+            return players
+        } else {
+            const playerOne = getPlayerName(playerOneId)
+            const playerTwo = getPlayerName(playerTwoId)
+            return [playerOne, playerTwo]
+        }
     }
-    
+
     if (players.length === 0) {
         return <div className="no-players-message">
             There are no players on this page
 
         </div>
     }
-    
+
     if (brackets === "2v2") {
-        {console.log(players)}
-        return (
-            <tr>
-                <th>Rank</th> <th>Region</th> <th>Player One</th> <th>Player Two</th>
-            </tr>
+        // { console.log(players) }
+        return (<table>
+            <tbody>
+
+
+                <tr>
+                    <th>Rank</th> <th>Region</th> <th>Player 1</th> <th>Player 2</th> <th>Rating</th> <th>Tier</th> <th>Wins</th> <th>Losses</th>
+                </tr>
+                {players.map((team) => {
+                    const [playerOne, playerTwo] = splitTeamNames(team.teamname, team.brawlhalla_id_one, team.brawlhalla_id_two)
+                    return <tr key={team.brawlhalla_id_one + team.brawlhalla_id_two}>
+                        <td>{team.rank}</td>
+                        <td>{team.region}</td>
+                        <td>{cleanString(playerOne)}</td>
+                        <td>{cleanString(playerTwo)}</td>
+                        <td>{team.rating}</td>
+                        <td>{team.tier}</td>
+                        <td>{team.wins}</td>
+                        <td>{team.games - team.wins}</td>
+
+                    </tr>
+                })}
+            </tbody>
+        </table>
         )
     }
     return (<table>
