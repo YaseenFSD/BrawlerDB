1335ff6c5770589fa6b7dcd176093d0644ce86df
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-12-22 11:35:31 -0500

    Search player is now on client side (non-functional)

--- client/src/pages/ranks/LeaderboardRanks.js
+++ client/src/pages/ranks/LeaderboardRanks.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { getRanks } from "../../api"
 import { usePaginatedQuery } from "react-query"
 import PrevAndNext from "../../components/prev-and-next/PrevAndNext"
@@ -7,42 +8,52 @@ import "./LeaderboardRanks.css";
 
 
 
-export const LeaderboardRanks = ({match:{params}}) => {
+export const LeaderboardRanks = ({ match: { params } }) => {
+    const [search, setSearch] = useState("")
     const { brackets, region, page } = params
     const { isLoading, isError, data, error, isFetching } = usePaginatedQuery(["ranks", brackets, region, page], getRanks)
+
+    const handleSearchPlayer = (e) => {
+        e.preventDefault()
+        // Todo use api call here to search player
+    }
+
     if (isError) {
         return <div className="error-message">Error: {error.message}</div>
     }
     if (isLoading) {
         return <div className="loading-message">Loading...</div>
     }
-    
+
     return (<div className="LeaderboardRanks">
-         <div className="nav-page">
-             <div className="nav-page-buttons-and-num">
-                 <div className="page-num"> Page:{page} </div>
-                 <PrevAndNext page={page} />
-                 {isFetching ? <div> Fetching...</div> : <div style={{ height: "1.2em" }}></div>}
-             </div>
-         </div>
+        <div className="nav-page">
+            <div className="nav-page-buttons-and-num">
+                <div className="page-num"> Page:{page} </div>
+                <PrevAndNext page={page} />
+                {isFetching ? <div> Fetching...</div> : <div style={{ height: "1.2em" }}></div>}
+            </div>
+            <form onSubmit={handleSearchPlayer}>
+                <input onChange={(e) => setSearch(e.target.value)} placeholder="Search player" className="search-bar"></input>
+            </form>
+        </div>
 
-         <LeaderBoardFilter region={region} brackets={brackets} page={page} />
+        <LeaderBoardFilter region={region} brackets={brackets} page={page} />
 
-         <LeaderboardTable brackets={brackets} players={data.data} page={page} />
-
-
-         <div className="nav-page-bottom">
-             <div className="nav-page-buttons-and-num">
-
-                 <PrevAndNext page={page}/>
-                 <div className="page-num"> Page:{page} </div>
-             </div>
-
-         </div>
+        <LeaderboardTable brackets={brackets} players={data.data} page={page} />
 
 
-     </div>)
-    
+        <div className="nav-page-bottom">
+            <div className="nav-page-buttons-and-num">
+
+                <PrevAndNext page={page} />
+                <div className="page-num"> Page:{page} </div>
+            </div>
+
+        </div>
+
+
+    </div>)
+
     // //Todo use a loading component where necessary
-    
+
 }
